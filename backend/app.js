b39d570e3845fe027e74eb6bb476a26254b46cb5
NEIR-0 <aalijlal.ganteng@gmail.com>
2025-04-01 15:00:20 +0700

    feat: router

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,16 @@
-if(!process.env.NODE_ENV) {
-    require('dotenv').config({path: ['.env']}) // kebutuhan deployment
+if (!process.env.NODE_ENV) {
+  require("dotenv").config({ path: [".env"] }); // kebutuhan deployment
 }
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000 // kebutuhan deployment
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000; // kebutuhan deployment
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// router
+const user = require("./router/user");
+
+app.use("/user", user);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
