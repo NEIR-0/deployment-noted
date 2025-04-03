c0d016f0d3df7f7634a7598159a6c4c67f9c1909
NEIR-0 <aalijlal.ganteng@gmail.com>
2025-04-03 20:08:23 +0700

    fix: be msg

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,7 +1,7 @@
 class ControllerUser {
   static async createUser(req, res, next) {
     console.log("masuk sini");
-    res.status(200).json({ message: "success" });
+    res.status(200).json({ message: "success changed auto build github" });
     try {
     } catch (error) {
       console.log(error);
